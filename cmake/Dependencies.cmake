05bbf6931c4b8482ec678a31239fd536a6e9156b
Michele Ferrari <m.ferrari092@studenti.unibs.it>
2023-12-21 19:07:42 +0100

    big reorganization of CMake dependencies

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,19 +2,19 @@ include(ExternalProject)
 set(ilqgames_LIBRARIES "")
 
 # Find Eigen.
-find_package( Eigen3 REQUIRED )
-include_directories(SYSTEM ${EIGEN3_INCLUDE_DIR})
-list(APPEND ilqgames_LIBRARIES ${EIGEN3_LIBRARIES})
+#find_package( Eigen3 REQUIRED )
+#include_directories(SYSTEM ${EIGEN3_INCLUDE_DIR})
+#list(APPEND ilqgames_LIBRARIES ${EIGEN3_LIBRARIES})
 
 # Find Google-gflags.
-include("cmake/External/gflags.cmake")
-include_directories(SYSTEM ${GFLAGS_INCLUDE_DIRS})
-list(APPEND ilqgames_LIBRARIES ${GFLAGS_LIBRARIES})
+#include("cmake/External/gflags.cmake")
+#include_directories(SYSTEM ${GFLAGS_INCLUDE_DIRS})
+#list(APPEND ilqgames_LIBRARIES ${GFLAGS_LIBRARIES})
 
 # Find Google-glog.
-include("cmake/External/glog.cmake")
-include_directories(SYSTEM ${GLOG_INCLUDE_DIRS})
-list(APPEND ilqgames_LIBRARIES ${GLOG_LIBRARIES})
+#include("cmake/External/glog.cmake")
+#include_directories(SYSTEM ${GLOG_INCLUDE_DIRS})
+#list(APPEND ilqgames_LIBRARIES ${GLOG_LIBRARIES})
 
 # Find OpenGL.
 find_package( OpenGL REQUIRED )
